9b374533343e328002988cc162081273b9afff59
John Ericson <Ericson2314@Yahoo.com>
2016-08-03 16:13:43 -0700

    Safer page table code

--- tests/kvm/bin.rs
+++ tests/kvm/bin.rs
@@ -1,4 +1,4 @@
-#![feature(linkage, naked_functions, asm)]
+#![feature(linkage, naked_functions, asm, const_fn)]
 // In this example we will construct a single CPU x86 VM which will execute
 // "inb 0x01" at ring 0
 
@@ -10,7 +10,7 @@ use kvm::{Capability, Exit, IoDirection, Segment, System, Vcpu, VirtualMachine};
 use memmap::{Mmap, Protection};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use x86::current::paging::*;
+use x86::bits64::paging::*;
 use x86::shared::paging::*;
 
 #[naked]
@@ -23,14 +23,22 @@ fn io_example() {
     let vaddr = VAddr::from_usize(&use_the_port as *const _ as _);
     println!("{} {}", pml4_index(vaddr), pdpt_index(vaddr));
 
-    // set up a page table that identity maps the lower half of the address space
+    static PAGE_TABLE_P: PAddr = PAddr::from_u64(0x1000);
+
+    // Set up a page table that identity maps the lower half of the address space
     let mut anon_mmap = Mmap::anonymous(2 * (1 << 20), Protection::ReadWrite).unwrap();
-    let slice = unsafe { anon_mmap.as_mut_slice() };
-    let pml4: &mut PML4 = unsafe { ::std::mem::transmute(&mut slice[0x1000]) };
+    let page_table_memory = unsafe { anon_mmap.as_mut_slice() };
+
+    type PageTable = (PML4, [PDPT; 256]);
+
+    let page_table: &mut PageTable = unsafe {
+        ::std::mem::transmute(&mut page_table_memory[PAGE_TABLE_P.as_u64() as usize])
+    };
+    let (ref mut pml4, ref mut pdpts) = *page_table;
     for i in 0..256 {
         let offset = 0x2000 + 0x1000 * i;
         pml4[i] = PML4Entry::new(PAddr::from_u64(offset as _), PML4_P | PML4_RW);
-        let pdpt: &mut PDPT = unsafe { ::std::mem::transmute(&mut slice[offset]) };
+        let pdpt = &mut pdpts[i];
         for j in 0..512 {
             pdpt[j] = PDPTEntry::new(PAddr::from_u64(((512 * i + j) as u64) << 30),
                                      PDPT_P | PDPT_RW | PDPT_PS);
@@ -41,8 +49,8 @@ fn io_example() {
         }
     }
 
-    slice[0x1f0000] = 0xe4;
-    slice[0x1f0001] = 0x01;
+    page_table_memory[0x1f0000] = 0xe4;
+    page_table_memory[0x1f0001] = 0x01;
 
     // Initialize the KVM system
     let sys = System::initialize().unwrap();
@@ -53,8 +61,13 @@ fn io_example() {
     // Ensure that the VM supports memory backing with user memory
     assert!(vm.check_capability(Capability::UserMemory) > 0);
 
-    vm.set_user_memory_region(0, slice, 0).unwrap();
+    // Once the memory is set we can't even call length.
+    let page_table_memory_limit = page_table_memory.len() - 1;
 
+    // Map the page table memory
+    vm.set_user_memory_region(0, page_table_memory, 0).unwrap();
+
+    // Map the process
     let f = File::open("/proc/self/maps").unwrap();
     let reader = BufReader::new(f);
 
@@ -72,6 +85,8 @@ fn io_example() {
                 let begin_ptr: *mut u8 = begin as *const u8 as _;
                 unsafe { ::std::slice::from_raw_parts_mut(begin_ptr, end - begin) }
             };
+            // Make sure process doesn't overlap with page table
+            assert!(begin > page_table_memory_limit);
             vm.set_user_memory_region(begin as _, slice, 0).unwrap();
         }
     }
